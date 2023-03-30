1238d09ecd55fa6b9308d04abf1b5b789cf3e88d
KRChapman <splash004@gmail.com>
2023-03-29 23:46:24 -0700

    refactor file names

--- src/main/java/com/restaurantExplorer/RestaurantExplorerApplication.java
+++ src/main/java/com/restaurantExplorer/RestaurantExplorerApplication.java
@@ -1,4 +1,4 @@
-package com.kyle.restaurantExplorer;
+package com.restaurantExplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
