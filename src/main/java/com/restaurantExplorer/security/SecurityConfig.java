32598909f2b81843f15b8d01a2dc760f44372b9d
KRChapman <splash004@gmail.com>
2023-04-02 20:50:41 -0700

    set up enviroment variables

---
+++ src/main/java/com/restaurantExplorer/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.restaurantexplorer.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	  private final JwtAuthenticationFilter jwtAuthFilter;
+	  private final AuthenticationProvider authenticationProvider;
+	  public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider
+			) { 
+		super();
+		this.jwtAuthFilter = jwtAuthFilter;
+		this.authenticationProvider = authenticationProvider;
+	}
+
+	@Bean
+	  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf()
+	        .disable()
+	        .authorizeHttpRequests()
+	        .requestMatchers("/auth/**")
+	          .permitAll()
+	          .requestMatchers(PathRequest.toH2Console()).permitAll()
+	        .anyRequest()
+	          .authenticated()
+	        .and()
+	          .sessionManagement()
+	          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        .and()
+	        .authenticationProvider(authenticationProvider)
+	        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+	
+	    .headers(header -> {header.
+	        frameOptions().sameOrigin();});
+	    return http.build();
+	  }
+}
