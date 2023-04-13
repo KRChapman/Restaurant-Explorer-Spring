38479855db337c6c687bb0ffa37eb4f4da90c6a4
KRChapman <splash004@gmail.com>
2023-04-13 12:42:56 -0700

    fix security api

--- src/main/java/com/restaurantExplorer/restaurant/RestaurantResource.java
+++ src/main/java/com/restaurantExplorer/restaurant/RestaurantResource.java
@@ -1,8 +1,11 @@
 package com.restaurantexplorer.restaurant;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 @RestController
 @RequestMapping("/api")
 public class RestaurantResource {
-
+	@Autowired
+	Environment environment;
+	
 	@GetMapping("/users/{username}/restaurants")
-   //
 	public List<RestaurantDTO> getRestaurants(@PathVariable String username) {
 		RestaurantDTO restaruant = new RestaurantDTO("TestFoo");
 	
@@ -26,4 +37,42 @@ public class RestaurantResource {
 				//
 		
 	}
+	
+	@GetMapping("/restaurants")
+	   //
+		public String getAllRestaurants() {
+		 String KEY = environment.getProperty("GOOGLE_KEY");
+
+			OkHttpClient client = new OkHttpClient().newBuilder()
+					  .build();
+					MediaType mediaType = MediaType.parse("text/plain");
+//					@SuppressWarnings("deprecation")
+//					RequestBody body = RequestBody.create(mediaType, "");
+					Request request = new Request.Builder()
+					  .url("https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key="+KEY)
+					
+					 .get()
+					 
+					//  .method("GET", body)
+					  .build();
+					Response	response = null; 
+					String data = "";
+					try {
+						response	= client.newCall(request).execute();
+						data = response.body().string();
+			     //     System.out.print(data);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		            return data;
+					
+					//new ResponseEntity<RestaurantDTO>(restaruant, HttpStatus.OK);
+			//	
+					//
+			
+		}
+		
+	
+	
 }
